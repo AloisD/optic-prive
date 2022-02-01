baf26cca922e13d4706723c81db5bca2eedb13fc
johnnyhelloworld <johnny58723826@hotmail.fr>
2022-02-01 12:07:07 +0100

    create segment table and fixtures

--- optic-prive-back/src/Entity/Segment.php
+++ optic-prive-back/src/Entity/Segment.php
@@ -15,8 +15,38 @@ class Segment
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $logo;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(?string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
     }
 }
