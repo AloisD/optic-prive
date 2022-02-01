4b6e92556a1c756ee2e81043e7582f7da18ad160
Stebou <stebou8mendy@gmail.com>
2022-02-01 02:55:12 +0100

    Create product service and use it

---
+++ optic-prive-front/src/app/services/product/product.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from 'src/app/models/IProduct';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private $url = 'https://127.0.0.1:8000/api';
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.$url}/products`);
+  }
+}
