fb2be348a6cb5137b527933e83efeeda8562a281
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-02-02 08:39:40 +0100

    added new crud controllers

--- optic-prive-back/src/Controller/Admin/ProductCrudController.php
+++ optic-prive-back/src/Controller/Admin/ProductCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
