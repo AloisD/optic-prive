bc2da01b2e54785f3c6116220281c8cf3f6ec2c4
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-02-02 10:56:34 +0100

    added a choice field to read and update the order status enum

--- optic-prive-back/src/Controller/Admin/OrderCrudController.php
+++ optic-prive-back/src/Controller/Admin/OrderCrudController.php
@@ -4,9 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Order;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class OrderCrudController extends AbstractCrudController
 {
@@ -18,10 +18,10 @@ class OrderCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            DateField::new('created_at'),
-            DateField::new('updated_at'),
-            TextField::new('order_status'),
+            IdField::new('id')->hideOnForm(),
+            DateTimeField::new('created_at'),
+            DateTimeField::new('updated_at'),
+            ChoiceField::new('order_status')->setChoices(['Cancelled' => 'orderCancelled', 'Delivered' => 'orderDelivered', 'In Transit' => 'orderInTransit', 'Payment Due' => 'orderPaymentDue', 'Pickup Available' => 'orderPickupAvailable', 'Problem' => 'orderProblem', 'Processing' => 'orderProcessing', 'Returned' => 'orderReturned']),
         ];
     }
 }
