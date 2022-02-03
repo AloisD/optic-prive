b7c5985d4bd0705a09f4415b0158bbd0020a0224
Stebou <stebou8mendy@gmail.com>
2022-02-03 02:00:56 +0100

    Update product and brand fixtures

--- optic-prive-back/src/DataFixtures/BrandFixtures.php
+++ optic-prive-back/src/DataFixtures/BrandFixtures.php
@@ -8,18 +8,26 @@ use Doctrine\Persistence\ObjectManager;
 
 class BrandFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        $faker = \Faker\Factory::create();
+  public const BRAND_RAY_BAN = "ray-ban";
+  public const BRAND_DOLCE_GABBANA = "dolce gabbana";
 
-        for ($p = 0; $p < random_int(5, 15); $p++) {
-            $brand = new Brand();
-            $brand
-                ->setName($faker->word())
-                ->setLogo($faker->imageUrl(640, 480, 'animals', true));
+  public function load(ObjectManager $manager): void
+  {
 
-                $manager->persist($brand);
-        }
-        $manager->flush();
-    }
+    $brand1 = new Brand();
+    $brand1
+      ->setName("ray-ban")
+      ->setLogo("https://www.pngmart.com/files/5/Ray-Ban-Logo-PNG-File.png");
+    $this->setReference(self::BRAND_RAY_BAN, $brand1);
+    $manager->persist($brand1);
+
+    $brand2 = new Brand();
+    $brand2
+      ->setName("dolce gabbana")
+      ->setLogo("https://i.pinimg.com/originals/36/3c/29/363c29434e634cb6f9f39d49bfcfe7f2.png");
+    $this->setReference(self::BRAND_DOLCE_GABBANA, $brand2);
+    $manager->persist($brand2);
+
+    $manager->flush();
+  }
 }
---
+++ optic-prive-back/src/DataFixtures/Scenario1Fixtures.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class Scenario1Fixtures extends Fixture implements DependentFixtureInterface
+{
+
+  public function load(ObjectManager $manager): void
+  {
+    $faker = \Faker\Factory::create();
+
+    $product = new Product();
+    $product
+      ->setName("aviator classic")
+      ->setReference('RB3025 L0205 58-14')
+      ->setColorCode('poli or:vert') // monture color : glasses color
+      ->setRetailPrice($faker->randomFloat(1, 10, 30))
+      ->setSellingPrice($faker->randomFloat(1, 10, 30))
+      ->setQuantity($faker->randomNumber(1, true))
+      ->setCreatedAt(new \DateTimeImmutable())
+      ->setUpdatedAt(new \DateTimeImmutable());
+
+    $this->getDataProduct($product, $faker);
+
+    //foreigns keys
+    $product
+      ->setBrand(($this->getReference(BrandFixtures::BRAND_RAY_BAN)));
+
+    $manager->persist($product);
+    $manager->flush();
+  }
+
+  private function getDataProduct(Product $product, $faker)
+  {
+    $state = $this->getStatusRandom();
+    $product->setState($state);
+
+    $category = $this->getCategoryRandom();
+    $product->setCategory($category);
+
+    $uvProtection = $this->getUvProtectionRandom();
+    $product->setUvProtection($uvProtection);
+
+    $itemAvailability = $this->getItemAvailabilityRandom();
+    $product->setItemAvailability($itemAvailability);
+
+    $eyeSize = $this->getEyeSizeRandom($faker);
+    $product->setEyeSize($eyeSize);
+
+    $bridgeSize = $this->getBridgeSizeRandom($faker);
+    $product->setBridgeSize($bridgeSize);
+
+    $templeLength = $this->getTempleLengthRandom($faker);
+    $product->setTempleLength($templeLength);
+  }
+
+  public function getDependencies()
+  {
+    return [
+      BrandFixtures::class
+    ];
+  }
+
+  /** Product *****************************************************************************/
+
+  // get random state
+  private function getStatusRandom(): string
+  {
+    $items = ['damagedCondition', 'newCondition', 'refurbishedCondition', 'usedCondition'];
+    return $items[random_int(0, 3)];
+  }
+
+  // get random category
+  private function getCategoryRandom(): string
+  {
+    $items = ['male', 'female', 'child', 'unisex'];
+    return $items[random_int(0, 3)];
+  }
+
+  // get random category
+  private function getUvProtectionRandom(): string
+  {
+    $items = ['category0', 'category1', 'category2', 'category3', 'category4'];
+    return $items[random_int(0, 4)];
+  }
+
+  private function getItemAvailabilityRandom(): string
+  {
+    $items = ['backOrder', 'discontinued', 'inStock', 'inStoreOnly', 'limitedAvailability', 'onlineOnly', 'outOfStock', 'preOrder', 'preSale', 'soldOut'];
+    return $items[random_int(0, 9)];
+  }
+
+  private function getEyeSizeRandom($faker)
+  {
+    return $faker->numberBetween(7, 20);
+  }
+
+  private function getBridgeSizeRandom($faker)
+  {
+    return $faker->numberBetween(7, 20);
+  }
+
+  private function getTempleLengthRandom($faker)
+  {
+    return $faker->numberBetween(5, 20);
+  }
+
+  /**END  Product************************************************************************* */
+}
