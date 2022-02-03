352be7ee6ff71be3b457fac3e659c69d005da54d
Stebou <stebou8mendy@gmail.com>
2022-02-03 23:10:35 +0100

    Update or create created_at and updated_at field into shippingOption and style

---
+++ optic-prive-back/migrations/Version20220203220855.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220203220855 extends AbstractMigration
+{
+  public function getDescription(): string
+  {
+    return 'Add created_at and updated_at into table style';
+  }
+
+  public function up(Schema $schema): void
+  {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE style ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+  }
+
+  public function down(Schema $schema): void
+  {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE style DROP created_at, DROP updated_at');
+  }
+}
--- optic-prive-back/src/Entity/ShippingOption.php
+++ optic-prive-back/src/Entity/ShippingOption.php
@@ -5,48 +5,52 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ShippingOptionRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
 
 #[ORM\Entity(repositoryClass: ShippingOptionRepository::class)]
 #[ApiResource]
-class ShippingOption
+class ShippingOption implements TimestampableInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private $id;
+  use TimestampableTrait;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $name;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private $id;
 
-    #[ORM\Column(type: 'decimal', precision: 5, scale: '2')]
-    private $price;
+  #[ORM\Column(type: 'string', length: 255)]
+  private $name;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  #[ORM\Column(type: 'decimal', precision: 5, scale: '2')]
+  private $price;
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
 
-        return $this;
-    }
+  public function setName(string $name): self
+  {
+    $this->name = $name;
 
-    public function getPrice(): ?string
-    {
-        return $this->price;
-    }
+    return $this;
+  }
 
-    public function setPrice(string $price): self
-    {
-        $this->price = $price;
+  public function getPrice(): ?string
+  {
+    return $this->price;
+  }
 
-        return $this;
-    }
+  public function setPrice(string $price): self
+  {
+    $this->price = $price;
+
+    return $this;
+  }
 }
