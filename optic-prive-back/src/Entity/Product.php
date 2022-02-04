1ed195b1a93cd23a4c75ac2e8d54d72134d8b4a5
Stebou <stebou8mendy@gmail.com>
2022-02-05 00:35:01 +0100

    Update product with api groups

--- optic-prive-back/src/Entity/Product.php
+++ optic-prive-back/src/Entity/Product.php
@@ -11,9 +11,25 @@ use Knp\DoctrineBehaviors\Contract\Entity\SluggableInterface;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Model\Sluggable\SluggableTrait;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+  collectionOperations: [
+    'get' => [
+      'normalization_context' => ['groups' => ['product_read']],
+    ],
+    "post"
+  ],
+  itemOperations: [
+    'get' => [
+      'normalization_context' => ['groups' => ['product_details_read']],
+    ],
+    'put',
+    'patch',
+    'delete',
+  ],
+)]
 class Product implements SluggableInterface, TimestampableInterface
 {
   use SluggableTrait;
@@ -24,6 +40,7 @@ class Product implements SluggableInterface, TimestampableInterface
   #[ORM\Column(type: 'integer')]
   private $id;
 
+  #[Groups(["product_read", "product_details_read"])]
   #[ORM\Column(type: 'string', length: 255)]
   private $name;
 
@@ -31,15 +48,18 @@ class Product implements SluggableInterface, TimestampableInterface
   private $reference;
 
   #[ORM\Column(type: 'string', length: 255)]
+  #[Groups(["product_details_read"])]
   private $color_code;
 
   #[ORM\Column(type: 'decimal', precision: 5, scale: '2')]
+  #[Groups(["product_read", "product_details_read"])]
   private $retail_price;
 
   #[ORM\Column(type: 'decimal', precision: 5, scale: '2')]
   private $selling_price;
 
   #[ORM\Column(type: 'integer')]
+  #[Groups(["product_details_read"])]
   private $quantity;
 
   #[ORM\Column(type: 'integer', nullable: true)]
@@ -99,8 +119,8 @@ class Product implements SluggableInterface, TimestampableInterface
 
   public function __construct()
   {
-      $this->productImages = new ArrayCollection();
-      $this->orderHasProducts = new ArrayCollection();
+    $this->productImages = new ArrayCollection();
+    $this->orderHasProducts = new ArrayCollection();
   }
 
   public function getId(): ?int
@@ -286,74 +306,74 @@ class Product implements SluggableInterface, TimestampableInterface
 
   public function getShape(): ?Shape
   {
-      return $this->shape;
+    return $this->shape;
   }
 
   public function setShape(?Shape $shape): self
   {
-      $this->shape = $shape;
+    $this->shape = $shape;
 
-      return $this;
+    return $this;
   }
 
   public function getSegment(): ?Segment
   {
-      return $this->segment;
+    return $this->segment;
   }
 
   public function setSegment(?Segment $segment): self
   {
-      $this->segment = $segment;
+    $this->segment = $segment;
 
-      return $this;
+    return $this;
   }
 
   public function getLensType(): ?LensType
   {
-      return $this->lens_type;
+    return $this->lens_type;
   }
 
   public function setLensType(?LensType $lens_type): self
   {
-      $this->lens_type = $lens_type;
+    $this->lens_type = $lens_type;
 
-      return $this;
+    return $this;
   }
 
   public function getStyle(): ?Style
   {
-      return $this->style;
+    return $this->style;
   }
 
   public function setStyle(?Style $style): self
   {
-      $this->style = $style;
+    $this->style = $style;
 
-      return $this;
+    return $this;
   }
 
   public function getColor(): ?Color
   {
-      return $this->color;
+    return $this->color;
   }
 
   public function setColor(?Color $color): self
   {
-      $this->color = $color;
+    $this->color = $color;
 
-      return $this;
+    return $this;
   }
 
   public function getMaterial(): ?Material
   {
-      return $this->material;
+    return $this->material;
   }
 
   public function setMaterial(?Material $material): self
   {
-      $this->material = $material;
+    $this->material = $material;
 
-      return $this;
+    return $this;
   }
 
   /**
@@ -361,29 +381,29 @@ class Product implements SluggableInterface, TimestampableInterface
    */
   public function getProductImages(): Collection
   {
-      return $this->productImages;
+    return $this->productImages;
   }
 
   public function addProductImage(ProductImage $productImage): self
   {
-      if (!$this->productImages->contains($productImage)) {
-          $this->productImages[] = $productImage;
-          $productImage->setProduct($this);
-      }
+    if (!$this->productImages->contains($productImage)) {
+      $this->productImages[] = $productImage;
+      $productImage->setProduct($this);
+    }
 
-      return $this;
+    return $this;
   }
 
   public function removeProductImage(ProductImage $productImage): self
   {
-      if ($this->productImages->removeElement($productImage)) {
-          // set the owning side to null (unless already changed)
-          if ($productImage->getProduct() === $this) {
-              $productImage->setProduct(null);
-          }
+    if ($this->productImages->removeElement($productImage)) {
+      // set the owning side to null (unless already changed)
+      if ($productImage->getProduct() === $this) {
+        $productImage->setProduct(null);
       }
+    }
 
-      return $this;
+    return $this;
   }
 
   /**
@@ -391,28 +411,28 @@ class Product implements SluggableInterface, TimestampableInterface
    */
   public function getOrderHasProducts(): Collection
   {
-      return $this->orderHasProducts;
+    return $this->orderHasProducts;
   }
 
   public function addOrderHasProduct(OrderHasProduct $orderHasProduct): self
   {
-      if (!$this->orderHasProducts->contains($orderHasProduct)) {
-          $this->orderHasProducts[] = $orderHasProduct;
-          $orderHasProduct->setProduct($this);
-      }
+    if (!$this->orderHasProducts->contains($orderHasProduct)) {
+      $this->orderHasProducts[] = $orderHasProduct;
+      $orderHasProduct->setProduct($this);
+    }
 
-      return $this;
+    return $this;
   }
 
   public function removeOrderHasProduct(OrderHasProduct $orderHasProduct): self
   {
-      if ($this->orderHasProducts->removeElement($orderHasProduct)) {
-          // set the owning side to null (unless already changed)
-          if ($orderHasProduct->getProduct() === $this) {
-              $orderHasProduct->setProduct(null);
-          }
+    if ($this->orderHasProducts->removeElement($orderHasProduct)) {
+      // set the owning side to null (unless already changed)
+      if ($orderHasProduct->getProduct() === $this) {
+        $orderHasProduct->setProduct(null);
       }
+    }
 
-      return $this;
+    return $this;
   }
 }
