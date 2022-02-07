23c40c1bd26f22c4e8e3b778f74138f23a8416b4
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-02-07 10:51:28 +0100

    added automated management of creation and update dates

--- optic-prive-back/src/Controller/Admin/OrderCrudController.php
+++ optic-prive-back/src/Controller/Admin/OrderCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -19,9 +20,19 @@ class OrderCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            DateTimeField::new('created_at'),
-            DateTimeField::new('updated_at'),
+            DateTimeField::new('created_at')->hideOnForm(),
+            DateTimeField::new('updated_at')->hideOnForm(),
             ChoiceField::new('order_status')->setChoices(['Cancelled' => 'orderCancelled', 'Delivered' => 'orderDelivered', 'In Transit' => 'orderInTransit', 'Payment Due' => 'orderPaymentDue', 'Pickup Available' => 'orderPickupAvailable', 'Problem' => 'orderProblem', 'Processing' => 'orderProcessing', 'Returned' => 'orderReturned']),
         ];
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof Order) return;
+
+        $entityInstance->setCreatedAt(new \DateTimeImmutable);
+        $entityInstance->setUpdatedAt(new \DateTimeImmutable);
+
+        parent::persistEntity($entityManager, $entityInstance);
     }
 }
