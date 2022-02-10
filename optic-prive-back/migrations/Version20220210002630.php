d28f98d15710b5b729683eef7183db2a3360c6c2
Stebou <stebou8mendy@gmail.com>
2022-02-10 01:42:25 +0100

    Created user entity/table

---
+++ optic-prive-back/migrations/Version20220210002630.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220210002630 extends AbstractMigration
+{
+  public function getDescription(): string
+  {
+    return 'Created user table';
+  }
+
+  public function up(Schema $schema): void
+  {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+  }
+
+  public function down(Schema $schema): void
+  {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->addSql('DROP TABLE user');
+    $this->addSql('ALTER TABLE address CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE recipient recipient VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE country country VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE city city VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE street street VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE company company VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE additionnal_details additionnal_details LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE brand CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE logo logo VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE color CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE logo logo VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE lens_type CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE material CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE logo logo VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE `order` CHANGE order_status order_status ENUM(\'orderCancelled\', \'orderDelivered\', \'orderInTransit\', \'orderPaymentDue\', \'orderPickupAvailable\', \'orderProblem\', \'orderProcessing\', \'orderReturned\') NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:enumOrderStatus)\'');
+    $this->addSql('ALTER TABLE order_has_product CHANGE order_has_product_status order_has_product_status ENUM(\'orderCancelled\', \'orderDelivered\', \'orderInTransit\', \'orderPaymentDue\', \'orderPickupAvailable\', \'orderProblem\', \'orderProcessing\', \'orderReturned\') NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:enumOrderStatus)\', CHANGE payment_info payment_info VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE shipping_company shipping_company VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE tracking_number tracking_number VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE product CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE reference reference VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE color_code color_code VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE state state ENUM(\'damagedCondition\', \'newCondition\', \'refurbishedCondition\', \'usedCondition\') NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:enumState)\', CHANGE category category ENUM(\'male\', \'female\', \'child\', \'unisex\') NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:enumCategory)\', CHANGE uv_protection uv_protection ENUM(\'category0\', \'category1\', \'category2\', \'category3\', \'category4\') DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:enumUvProtection)\', CHANGE item_availability item_availability ENUM(\'backOrder\', \'discontinued\', \'inStock\', \'inStoreOnly\', \'limitedAvailability\', \'onlineOnly\', \'outOfStock\', \'preOrder\', \'preSale\', \'soldOut\') NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:enumItemAvailability)\', CHANGE image_name image_name VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE slug slug VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE product_image CHANGE path path VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE segment CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE logo logo VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE shape CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE logo logo VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE shipping_option CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    $this->addSql('ALTER TABLE style CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+  }
+}
