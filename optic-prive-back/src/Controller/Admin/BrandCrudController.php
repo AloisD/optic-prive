555f8517f2a3f26648f0e82ab54d89e342f6913c
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-02-10 11:21:31 +0100

    added AssociationFields for brand, color, lensType, material, segment, shape and style into productCrudController

--- optic-prive-back/src/Controller/Admin/BrandCrudController.php
+++ optic-prive-back/src/Controller/Admin/BrandCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Brand;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class BrandCrudController extends AbstractCrudController
 {
@@ -12,14 +15,18 @@ class BrandCrudController extends AbstractCrudController
         return Brand::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name');
     }
-    */
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof Brand) return;
+
+        $entityInstance->setCreatedAt(new \DateTimeImmutable);
+        $entityInstance->setUpdatedAt(new \DateTimeImmutable);
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
 }
--- optic-prive-back/src/Controller/Admin/DashboardController.php
+++ optic-prive-back/src/Controller/Admin/DashboardController.php
@@ -5,7 +5,13 @@ namespace App\Controller\Admin;
 use App\Entity\Address;
 use App\Entity\Order;
 use App\Entity\Product;
-use App\Entity\ProductImage;
+use App\Entity\Brand;
+use App\Entity\Color;
+use App\Entity\LensType;
+use App\Entity\Material;
+use App\Entity\Segment;
+use App\Entity\Shape;
+use App\Entity\Style;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -32,11 +38,16 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
-/*         yield MenuItem::subMenu('Offer', 'fa fa-article')->setSubItems([
+        yield MenuItem::subMenu('Offer', 'fa fa-article')->setSubItems([
             MenuItem::linkToCrud('Product', 'fas fa-list', Product::class),
-            MenuItem::linkToCrud('Picture', 'fas fa-list', ProductImage::class)
-        ]); */
+            MenuItem::linkToCrud('Brand', 'fas fa-list', Brand::class),
+            MenuItem::linkToCrud('Shape', 'fas fa-list', Shape::class),
+            MenuItem::linkToCrud('Segment', 'fas fa-list', Segment::class),
+            MenuItem::linkToCrud('Lens type', 'fas fa-list', LensType::class),
+            MenuItem::linkToCrud('Style', 'fas fa-list', Style::class),
+            MenuItem::linkToCrud('Color', 'fas fa-list', Color::class),
+            MenuItem::linkToCrud('Material', 'fas fa-list', Material::class),
+        ]);
         yield MenuItem::linkToCrud('Order', 'fas fa-list', Order::class);
         yield MenuItem::linkToCrud('Address', 'fas fa-list', Address::class);
         yield MenuItem::linkToUrl('Visit public website', null, '/');
