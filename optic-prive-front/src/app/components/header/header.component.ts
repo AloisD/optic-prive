64d5e1669d62e4417e3522c614a7de49ab6cf871
johnnyhelloworld <johnny58723826@hotmail.fr>
2022-02-11 12:11:55 +0100

    display datas on home-page-component

--- optic-prive-front/src/app/components/header/header.component.ts
+++ optic-prive-front/src/app/components/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from 'src/app/models/IProduct';
-import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +6,9 @@ import { ProductService } from 'src/app/services/product/product.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  products!: [IProduct];
 
-  constructor(private productService: ProductService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((datas: any) => {
-      this.products = datas['hydra:member'];
-      console.log(this.products);
-    });
   }
 }
--- optic-prive-front/src/app/pages/home-page/home-page.component.ts
+++ optic-prive-front/src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IProduct } from 'src/app/models/IProduct';
+import { ProductService } from 'src/app/services/product/product.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  products!: [IProduct];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((datas: any) => {
+      this.products = datas['hydra:member'];
+      console.log(this.products);
+    });
   }
-
 }
