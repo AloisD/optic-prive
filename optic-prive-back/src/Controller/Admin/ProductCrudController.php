e5b74da242b977ec31fb687ae866b420df3e1a4e
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2022-02-15 10:25:30 +0100

    testing dashboard customization

--- optic-prive-back/src/Controller/Admin/ProductCrudController.php
+++ optic-prive-back/src/Controller/Admin/ProductCrudController.php
@@ -3,15 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ProductImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-
 class ProductCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -19,11 +19,12 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
         yield IdField::new('id')->hideOnForm();
         yield TextField::new('name');
+        yield CollectionField::new('productImages')
+          ->setEntryType(ProductImageType::class);
         yield AssociationField::new('brand');/* ->autocomplete() use autocomplete if you have too many values causing "out of memory" errors */
         yield TextField::new('reference');
         yield TextField::new('color_code')->hideOnIndex();
@@ -37,8 +38,8 @@ class ProductCrudController extends AbstractCrudController
         yield ChoiceField::new('UvProtection')->setChoices(['0' => 'category0', '1' => 'category1', '2' => 'category2', '3' => 'category3', '4' => 'category4'])->hideOnIndex();
         yield ChoiceField::new('ItemAvailability')->setChoices(['Discontinued' => 'discontinued', 'In stock' => 'inStock', 'Out of stock' => 'outOfStock'])->hideOnIndex();;
         yield ChoiceField::new('state')->setChoices(['Damaged' => 'damagedCondition', 'New' => 'newCondition', 'Refurbished' => 'refurbishedCondition', 'Used' => 'usedCondition']);
-        yield MoneyField::new('selling_price')->setCurrency('EUR');
-        yield MoneyField::new('retail_price')->setCurrency('EUR')->hideOnIndex();
+        yield NumberField::new('selling_price');
+        yield NumberField::new('retail_price')->hideOnIndex();
         yield NumberField::new('quantity');
         yield NumberField::new('eye_size')->hideOnIndex();
         yield NumberField::new('bridge_size')->hideOnIndex();
--- optic-prive-back/src/Entity/ProductImage.php
+++ optic-prive-back/src/Entity/ProductImage.php
@@ -7,9 +7,14 @@ use App\Repository\ProductImageRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 #[ORM\Entity(repositoryClass: ProductImageRepository::class)]
 #[ApiResource]
+/**
+ * @Vich\Uploadable
+ */
 class ProductImage implements TimestampableInterface
 {
   use TimestampableTrait;
@@ -21,6 +26,12 @@ class ProductImage implements TimestampableInterface
 
   #[ORM\Column(type: 'string', length: 255)]
   private $path;
+
+  /**
+   * @Vich\UploadableField(mapping="product_image", fileNameProperty="path")
+   * @var File
+   */
+  private $image;
 
   #[ORM\ManyToOne(targetEntity: Product::class, inversedBy: 'productImages')]
   #[ORM\JoinColumn(nullable: false)]
@@ -54,4 +65,36 @@ class ProductImage implements TimestampableInterface
 
       return $this;
   }
+
+  public function __toString()
+  {
+    return $this->getPath();
+  }
+
+
+  /**
+   * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+   * of 'UploadedFile' is injected into this setter to trigger the update. If this
+   * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+   * must be able to accept an instance of 'File' as the bundle will inject one here
+   * during Doctrine hydration.
+   *
+   * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+   */
+  public function setImage(?File $image = null): void
+  {
+    $this->image = $image;
+
+    if (null !== $image) {
+      // It is required that at least one field changes if you are using doctrine
+      // otherwise the event listeners won't be called and the file is lost
+      $this->updatedAt = new \DateTimeImmutable();
+    }
+  }
+
+  public function getImage(): ?File
+  {
+    return $this->image;
+  }
+
 }
