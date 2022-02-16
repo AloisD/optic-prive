d7a9f479a486e3de7ed1305f5a1cc7ba4b231534
Jon2may <jonathan.demay004@gmail.com>
2022-02-16 18:06:49 +0100

    registration page component created

--- optic-prive-front/src/app/app.module.ts
+++ optic-prive-front/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
 import { FormsModule } from '@angular/forms';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
 
 export function createTranslateLoader(httpClient: HttpClient) {
@@ -27,7 +28,8 @@ export function createTranslateLoader(httpClient: HttpClient) {
     BannerComponent,
     HomePageComponent,
     NavbarComponent,
-    CartPageComponent
+    CartPageComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
