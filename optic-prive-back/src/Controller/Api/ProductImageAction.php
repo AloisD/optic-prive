e0041c8a89676f0a7375a80df0ea65d8de02b3ff
Stebou <stebou8mendy@gmail.com>
2022-02-17 10:52:43 +0100

    Update method invoke of productImageAction controller

--- optic-prive-back/src/Controller/Api/ProductImageAction.php
+++ optic-prive-back/src/Controller/Api/ProductImageAction.php
@@ -21,10 +21,10 @@ final class ProductImageAction extends AbstractController
     if (!$uploadedFile) {
       throw new BadRequestHttpException('image is required');
     }
-    $data->setImageFile($uploadedFile);
     $productImage = new ProductImage();
-    $productImage->setPath($data->getImageName());
+    $productImage->setPath($uploadedFile->getClientOriginalName());
     $productImage->setProduct($data);
+    $data->addProductImage($productImage);
 
     return $data;
   }
