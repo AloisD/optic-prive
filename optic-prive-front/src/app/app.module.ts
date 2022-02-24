8b62bf6a1c8b70aa5a93b849e11d754fef7ab5cc
Nicolas Chalono <nicolas.chalono@gmail.com>
2022-02-24 15:06:53 +0100

    new component logo

--- optic-prive-front/src/app/app.module.ts
+++ optic-prive-front/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FormsModule } from '@angular/forms';
 import { ShippingPageComponent } from './pages/checkout/shipping-page/shipping-page.component';
 import { StepperComponent } from './components/stepper/stepper.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 
 
@@ -34,7 +35,8 @@ export function createTranslateLoader(httpClient: HttpClient) {
     CartPageComponent,
     ShippingPageComponent,
     StepperComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    LogoComponent
   ],
 
   imports: [
