c0733deb18d5bc58175ab5c22838097d331542e3
johnnyhelloworld <90685115+johnnyhelloworld@users.noreply.github.com>
2022-02-25 17:58:28 +0100

    Revert "add foreign key seller_id in table product"

--- optic-prive-back/src/Entity/Product.php
+++ optic-prive-back/src/Entity/Product.php
@@ -165,10 +165,6 @@ class Product implements SluggableInterface, TimestampableInterface
   #[ORM\OneToMany(mappedBy: 'product', targetEntity: OrderHasProduct::class)]
   private $orderHasProducts;
 
-  #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'products')]
-  #[ORM\JoinColumn(nullable: false)]
-  private $seller;
-
   public function __construct()
   {
     $this->productImages = new ArrayCollection();
@@ -491,17 +487,5 @@ class Product implements SluggableInterface, TimestampableInterface
   public function __toString()
   {
     return $this->name;
-  }
-
-  public function getSeller(): ?User
-  {
-      return $this->seller;
-  }
-
-  public function setSeller(?User $seller): self
-  {
-      $this->seller = $seller;
-
-      return $this;
   }
 }
