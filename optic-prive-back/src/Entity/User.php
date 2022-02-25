ab9138cfadfa8937b8170a8143f3c5ec2930e3f2
johnnyhelloworld <johnny58723826@hotmail.fr>
2022-02-25 13:08:26 +0100

    add foreign key buyer_id in table order

--- optic-prive-back/src/Entity/User.php
+++ optic-prive-back/src/Entity/User.php
@@ -60,10 +60,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Timesta
   #[ORM\OneToMany(mappedBy: 'user', targetEntity: Address::class)]
   private $addresses;
 
+  #[ORM\OneToMany(mappedBy: 'buyer', targetEntity: Order::class)]
+  private $orders;
+
   public function __construct()
   {
       $this->businessUsers = new ArrayCollection();
       $this->addresses = new ArrayCollection();
+      $this->orders = new ArrayCollection();
   }
 
   public function getId(): ?int
@@ -202,6 +206,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Timesta
           // set the owning side to null (unless already changed)
           if ($address->getUser() === $this) {
               $address->setUser(null);
+          }
+      }
+
+      return $this;
+  }
+
+  /**
+   * @return Collection<int, Order>
+   */
+  public function getOrders(): Collection
+  {
+      return $this->orders;
+  }
+
+  public function addOrder(Order $order): self
+  {
+      if (!$this->orders->contains($order)) {
+          $this->orders[] = $order;
+          $order->setBuyer($this);
+      }
+
+      return $this;
+  }
+
+  public function removeOrder(Order $order): self
+  {
+      if ($this->orders->removeElement($order)) {
+          // set the owning side to null (unless already changed)
+          if ($order->getBuyer() === $this) {
+              $order->setBuyer(null);
           }
       }
 
