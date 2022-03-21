9e5cb0b45e83f1c6be1f0142fd403dd2a345f232
Stebou <stebou8mendy@gmail.com>
2022-03-21 17:40:32 +0100

    Implement pagination

--- optic-prive-front/src/app/pages/home-page/home-page.component.ts
+++ optic-prive-front/src/app/pages/home-page/home-page.component.ts
@@ -10,22 +10,60 @@ import { ProductService } from 'src/app/services/product/product.service';
 })
 export class HomePageComponent implements OnInit {
   public products!: [IProduct];
+  public nextUrl!: string;
+  public previousUrl!: string;
 
   constructor(
     private productService: ProductService,
-    private cartService: CartService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
     this.productService.getLatestProducts().subscribe((datas: any) => {
+      //console.log('datas :', datas);
+
       this.products = datas['hydra:member'];
       this.products.forEach((product: any) => {
         Object.assign(product, { quantityOrdered: 0 });
       });
+      // For pagination
+      this.nextUrl = datas['hydra:view']['hydra:next'];
+      this.previousUrl = datas['hydra:view']['hydra:previous'];
     });
   }
 
   addtocart(product: any) {
     this.cartService.addToCart(product);
+  }
+
+  goToPreviousPage() {
+    this.productService
+      .getLatestProductsByUrl(this.previousUrl)
+      .subscribe((datas: any) => {
+        this.products = datas['hydra:member'];
+        this.products.forEach((product: any) => {
+          Object.assign(product, { quantityOrdered: 0 });
+        });
+
+        // For pagination
+        this.nextUrl = datas['hydra:view']['hydra:next'];
+        this.previousUrl = datas['hydra:view']['hydra:previous'];
+      });
+  }
+
+  goToNextPage() {
+    console.log('Next url', this.nextUrl);
+    this.productService
+      .getLatestProductsByUrl(this.nextUrl)
+      .subscribe((datas: any) => {
+        this.products = datas['hydra:member'];
+        this.products.forEach((product: any) => {
+          Object.assign(product, { quantityOrdered: 0 });
+        });
+
+        // For pagination
+        this.nextUrl = datas['hydra:view']['hydra:next'];
+        this.previousUrl = datas['hydra:view']['hydra:previous'];
+      });
   }
 }
