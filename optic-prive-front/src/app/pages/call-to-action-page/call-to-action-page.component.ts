0d89675d1a2705480060831462a0edb1dd6cd2d4
Jon2may <jonathan.demay004@gmail.com>
2022-03-23 11:18:19 +0100

    paging added in call-to-action page

--- optic-prive-front/src/app/pages/call-to-action-page/call-to-action-page.component.ts
+++ optic-prive-front/src/app/pages/call-to-action-page/call-to-action-page.component.ts
@@ -10,6 +10,8 @@ import { ProductService } from 'src/app/services/product/product.service';
 })
 export class CallToActionPageComponent implements OnInit {
   public products!: [IProduct];
+  public nextUrl!: string;
+  public previousUrl!: string;
 
   constructor(
     private productService: ProductService,
@@ -22,10 +24,44 @@ export class CallToActionPageComponent implements OnInit {
       this.products.forEach((product: any) => {
         Object.assign(product, { quantityOrdered: 0 });
       });
+       // // For pagination
+       this.nextUrl = datas['hydra:view']['hydra:next'];
+       this.previousUrl = datas['hydra:view']['hydra:previous'];
     });
   }
 
   addtocart(product: any) {
     this.cartService.addToCart(product);
+  }
+
+  goToPreviousPage() {
+    this.productService
+      .getLatestProductsByUrl(this.previousUrl)
+      .subscribe((datas: any) => {
+        this.products = datas['hydra:member'];
+        this.products.forEach((product: any) => {
+          Object.assign(product, { quantityOrdered: 0 });
+        });
+
+        // For pagination
+        this.nextUrl = datas['hydra:view']['hydra:next'];
+        this.previousUrl = datas['hydra:view']['hydra:previous'];
+      });
+  }
+
+  goToNextPage() {
+    console.log('Next url', this.nextUrl);
+    this.productService
+      .getLatestProductsByUrl(this.nextUrl)
+      .subscribe((datas: any) => {
+        this.products = datas['hydra:member'];
+        this.products.forEach((product: any) => {
+          Object.assign(product, { quantityOrdered: 0 });
+        });
+
+        // For pagination
+        this.nextUrl = datas['hydra:view']['hydra:next'];
+        this.previousUrl = datas['hydra:view']['hydra:previous'];
+      });
   }
 }
